985e31db9a2e3208577de67f20fccacd48d5a8a1
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2020-12-11 11:41:29 +0100

    geändert

--- Aufgabe3.1/script.js
+++ Aufgabe3.1/script.js
@@ -24,7 +24,7 @@ var P_3_1Server;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
-        console.log();
+        console.log(_request.url);
         _response.end();
         // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
--- Aufgabe3.1/script.ts
+++ Aufgabe3.1/script.ts
@@ -25,7 +25,7 @@ export namespace P_3_1Server {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
-        console.log();
+        console.log(_request.url);
         _response.end();
         // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
