ecd47bfe40985070b87e4078a25515fa34bc9a35
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2020-12-11 09:10:51 +0100

    Kommentare hinzugefügt

--- Aufgabe3.1/script.js
+++ Aufgabe3.1/script.js
@@ -1,13 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
+//Http Modul importieren
 const Http = require("http");
 var P_3_1Server;
 (function (P_3_1Server) {
     console.log("Starting server");
+    //environment mit der Angabe der Portnummer von heroku
     let port = Number(process.env.PORT);
+    //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
     if (!port)
         port = 8100;
+    //Server erstellen
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -20,7 +24,9 @@ var P_3_1Server;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
+        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe3.1/script.ts
+++ Aufgabe3.1/script.ts
@@ -1,11 +1,15 @@
+//Http Modul importieren
 import * as Http from "http";
 
 export namespace P_3_1Server {
     console.log("Starting server");
+    //environment mit der Angabe der Portnummer von heroku
     let port: number = Number(process.env.PORT);
+    //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
     if (!port)
         port = 8100;
 
+    //Server erstellen
     let server: Http.Server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -21,6 +25,8 @@ export namespace P_3_1Server {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
+        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
 }
