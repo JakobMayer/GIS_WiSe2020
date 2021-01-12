f4938f38b904cf35e28bc117d2c64c0e3dd17aac
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2021-01-12 21:57:20 +0100

    geändert

--- Aufgabe3.1/client.ts
+++ Aufgabe3.1/client.ts
@@ -1,5 +1,5 @@
 
-/*
+
 let registerForm: HTMLFormElement = <HTMLFormElement>document.getElementById("register-form");
 
 let registerButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send-button");
@@ -20,41 +20,4 @@ async function submitToServer(_event: Event): Promise<void> {
     alert("Antwort: " + responseText);
 
 
-}
-*/
-
-let formularForm: HTMLFormElement = <HTMLFormElement>document.getElementById("register-form");
-
-let registerButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send-button");
-registerButton.addEventListener("click", submitToServer);
-
-let url: string = "https://gisapplication.herokuapp.com/";
-
-
-async function submitToServer(_event: Event): Promise<void> {
-
-    let formData: FormData = new FormData(document.forms[0]);
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-
-
-    url = url + "?" + query.toString();
-    console.log(url);
-    
-
-    let response: Response = await fetch(url + "?" + query.toString());
-    let responseText: string = await response.text();
-    console.log(response);
-    alert(responseText);
-
-}
-
-async function communicate(_url: RequestInfo): Promise<void> {
-    let response: Response = await fetch(_url);
-    console.log("Response", response);
-  }
-
-function processData (_event: Event): void { 
-
-    
-    console.log();
 }
--- Aufgabe3.1/server.js
+++ Aufgabe3.1/server.js
@@ -1,64 +1,18 @@
 "use strict";
-/*
-//Http Modul importieren
-import * as Http from "http";
-import * as Url from "url";
-
-export namespace P_3_1Server {
-    console.log("Starting server");
-    //environment mit der Angabe der Portnummer von heroku
-    let port: number = Number(process.env.PORT);
-    //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
-    if (!port)
-        port = 8100;
-
-    //Server erstellen
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
-
-    function handleListen(): void {
-        console.log("Listening");
-    }
-
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-
-        if (_request.url) {
-            let q: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            
-            for (let key in q.query) {
-                _response.write (key + ":" + q.query[key] + "<br/>");
-            }
-    
-            let stringJSON: string = JSON.stringify(q.query);
-            _response.write(stringJSON);
-        }
-        
-        console.log(_request.url);
-        _response.end();
-        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
-    }
-}
-*/
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
+//Http Modul importieren
 const Http = require("http");
 const Url = require("url");
 var P_3_1Server;
 (function (P_3_1Server) {
     console.log("Starting server");
-    //enviroment mit der Angabe der Portnummer von Heroku
+    //environment mit der Angabe der Portnummer von heroku
     let port = Number(process.env.PORT);
-    //wenn port nicht definiert ist (keiner von heroku zugewiesen wurde), wird port nr. 8100 aufgerufen
+    //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
     if (!port)
         port = 8100;
-    //server wird erstellt    
+    //Server erstellen
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -70,6 +24,7 @@ var P_3_1Server;
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
         if (_request.url) {
             let q = Url.parse(_request.url, true);
             for (let key in q.query) {
@@ -78,8 +33,9 @@ var P_3_1Server;
             let stringJSON = JSON.stringify(q.query);
             _response.write(stringJSON);
         }
+        console.log(_request.url);
         _response.end();
-        //ein neuer Header wird erstellt und dort das request auf einer neuen Seite ausgegeben
+        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=server.js.map
