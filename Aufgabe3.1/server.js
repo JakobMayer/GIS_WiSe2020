b56d972d9951a95d1c92a68ca370a974fbcf67c5
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2021-01-12 20:34:26 +0100

    geändert

--- Aufgabe3.1/server.js
+++ Aufgabe3.1/server.js
@@ -1,33 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.P_3_1Server = void 0;
 //Http Modul importieren
-import * as Http from "http";
-
-export namespace P_3_1Server {
+const Http = require("http");
+var P_3_1Server;
+(function (P_3_1Server) {
     console.log("Starting server");
     //environment mit der Angabe der Portnummer von heroku
-    let port: number = Number(process.env.PORT);
+    let port = Number(process.env.PORT);
     //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
     if (!port)
         port = 8100;
-
     //Server erstellen
-    let server: Http.Server = Http.createServer();
+    let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
-
-    function handleListen(): void {
+    function handleListen() {
         console.log("Listening");
     }
-
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    function handleRequest(_request, _response) {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        
+        _response.write("was geht?");
         console.log(_request.url);
         _response.end();
         // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
     }
-}
+})(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
+//# sourceMappingURL=server.js.map
---
+++ Aufgabe3.1/server.ts
@@ -0,0 +1,34 @@
+//Http Modul importieren
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace P_3_1Server {
+    console.log("Starting server");
+    //environment mit der Angabe der Portnummer von heroku
+    let port: number = Number(process.env.PORT);
+    //Wenn der port nicht definiert ist bzw keiner von heroku zugewiesen wurde, wird der port nr. 8100 aufgerufen
+    if (!port)
+        port = 8100;
+
+    //Server erstellen
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write("was geht?");
+        
+        console.log(_request.url);
+        _response.end();
+        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+    }
+}
