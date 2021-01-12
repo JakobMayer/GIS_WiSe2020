7413b23b03def3a09077526a88699139bc6d5558
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2021-01-12 21:54:26 +0100

    geändert

--- Aufgabe3.1/server.ts
+++ Aufgabe3.1/server.ts
@@ -1,3 +1,4 @@
+/*
 //Http Modul importieren
 import * as Http from "http";
 import * as Url from "url";
@@ -41,5 +42,47 @@ export namespace P_3_1Server {
         console.log(_request.url);
         _response.end();
         // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+    }
+}
+*/
+
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace P_3_1Server {
+    console.log("Starting server");
+    //enviroment mit der Angabe der Portnummer von Heroku
+    let port: number = Number(process.env.PORT);
+    //wenn port nicht definiert ist (keiner von heroku zugewiesen wurde), wird port nr. 8100 aufgerufen
+    if (!port)
+        port = 8100;
+
+    //server wird erstellt    
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (_request.url) {
+            let q: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            
+            for (let key in q.query) {
+                _response.write (key + ":" + q.query[key] + "<br/>");
+            }
+    
+            let stringJSON: string = JSON.stringify(q.query);
+            _response.write(stringJSON);
+        }
+        _response.end();
+        //ein neuer Header wird erstellt und dort das request auf einer neuen Seite ausgegeben
     }
 }
