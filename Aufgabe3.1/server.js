f9769e6b6cc6a952eb195fc9f2214ddf6f8062d7
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2021-01-12 20:36:29 +0100

    geändert

--- Aufgabe3.1/server.js
+++ Aufgabe3.1/server.js
@@ -23,7 +23,7 @@ var P_3_1Server;
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("was geht?");
+        _response.write("Was geht?");
         console.log(_request.url);
         _response.end();
         // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
--- Aufgabe3.1/server.ts
+++ Aufgabe3.1/server.ts
@@ -25,7 +25,7 @@ export namespace P_3_1Server {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("was geht?");
+        _response.write("Was geht?");
         
         console.log(_request.url);
         _response.end();
