0f9e7b98658c7043c5d5843284dead255d4cab97
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2021-01-21 22:45:13 +0100

    registrieren und anmelden

--- Aufgabe3.1/server.js
+++ Aufgabe3.1/server.js
@@ -7,11 +7,6 @@ const Url = require("url");
 const Mongo = require("mongodb");
 var P_3_1Server;
 (function (P_3_1Server) {
-    async function conectMongo() {
-        let mongoClient = new Mongo.MongoClient("localhost:27017");
-        await mongoClient.connect();
-    }
-    conectMongo();
     console.log("Starting server");
     //environment mit der Angabe der Portnummer von heroku
     let port = Number(process.env.PORT);
@@ -23,6 +18,15 @@ var P_3_1Server;
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
+    let mongoUrl = "mongodb+srv://Beispiel_User:<password>@cluster2000.9tkvz.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    let user;
+    async function conectMongo(_url) {
+        let mongoClient = new Mongo.MongoClient("localhost:27017");
+        await mongoClient.connect();
+        console.log("Mongo verbunden");
+        user = mongoClient.db("Formulare").collection("User");
+    }
+    conectMongo(mongoUrl);
     function handleListen() {
         console.log("Listening");
     }
@@ -32,21 +36,49 @@ var P_3_1Server;
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let q = Url.parse(_request.url, true);
-            console.log(q.pathname);
             if (q.pathname == "/index") {
-                for (let key in q.query) {
+                /*  for (let key in q.query) {
                     _response.write(key + ":" + q.query[key] + "<br/>");
+                } */
+                let queryParameters = q.query;
+                let user = {
+                    "vorname": queryParameters.vorname,
+                    "nachname": queryParameters.nachname,
+                    "email": queryParameters.email,
+                    "passwort": queryParameters.passwort
+                };
+                await registriereUser(user);
+                if (q.pathname == "/einloggen") {
+                    //let stringJSON: string = JSON.stringify(q.query);
+                    //_response.write(stringJSON);
+                    let queryParameters = q.query;
+                    await anmelden(queryParameters.email, queryParameters.passwort);
                 }
             }
-            if (q.pathname == "/json") {
-                let stringJSON = JSON.stringify(q.query);
-                _response.write(stringJSON);
+            _response.end();
+            // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+        }
+        async function registriereUser(_user) {
+            //überprüfen ob es schon ein Konto mit der Mailadresse gibt
+            //countDocuments
+            let countDocuments = await user.countDocuments({ "email": _user.email });
+            if (countDocuments > 0) {
+                console.log("Mailadresse bereits vergeben");
+                //TODO: An Client weitergeben
+            }
+            else {
+                await user.insertOne(_user);
             }
         }
-        _response.end();
-        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+        async function anmelden(_email, _passwort) {
+            let countDocuments = await user.countDocuments({ "email": _email, "passwort": _passwort });
+            if (countDocuments > 0) {
+                console.log("angemeldet");
+            }
+            else {
+                console.log("falsche Daten eingegeben");
+            }
+        }
     }
-    let databaseUrl = "mongodb+srv://Beispiel_User:<password>@cluster2000.9tkvz.mongodb.net/<dbname>?retryWrites=true&w=majority";
-    let user;
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=server.js.map
--- Aufgabe3.1/server.ts
+++ Aufgabe3.1/server.ts
@@ -6,13 +6,6 @@ import * as Mongo from "mongodb";
 
 export namespace P_3_1Server {
 
-    async function conectMongo(): Promise<void> {
-        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient("localhost:27017");
-        await mongoClient.connect();
-    }
-    conectMongo();
-
-
     console.log("Starting server");
     //environment mit der Angabe der Portnummer von heroku
     let port: number = Number(process.env.PORT);
@@ -26,9 +19,30 @@ export namespace P_3_1Server {
     server.addListener("listening", handleListen);
     server.listen(port);
 
+    interface User {
+        "vorname": string;
+        "nachname": string;
+        "email": string;
+        "passwort": string;
+    }
+
+    let mongoUrl: string = "mongodb+srv://Beispiel_User:<password>@cluster2000.9tkvz.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    let user: Mongo.Collection;
+
+
+    async function conectMongo(_url: string): Promise<void> {
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient("localhost:27017");
+        await mongoClient.connect();
+        console.log("Mongo verbunden");
+        user = mongoClient.db("Formulare").collection("User");
+    }
+    conectMongo(mongoUrl);
+
+
     function handleListen(): void {
         console.log("Listening");
     }
+
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear you!");
@@ -37,34 +51,67 @@ export namespace P_3_1Server {
 
         if (_request.url) {
             let q: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            console.log(q.pathname);
+
 
             if (q.pathname == "/index") {
-                for (let key in q.query) {
+                /*  for (let key in q.query) {
                     _response.write(key + ":" + q.query[key] + "<br/>");
+                } */
+                let queryParameters: Query = <Query>q.query;
+
+                let user: User = {
+
+                    "vorname": queryParameters.vorname as string,
+                    "nachname": queryParameters.nachname as string,
+                    "email": queryParameters.email as string,
+                    "passwort": queryParameters.passwort as string
+                };
+
+                await registriereUser(user);
+
+
+
+                if (q.pathname == "/einloggen") {
+                    //let stringJSON: string = JSON.stringify(q.query);
+                    //_response.write(stringJSON);
+
+                    let queryParameters: Query = <Query>q.query;
+                    await anmelden(queryParameters.email as string, queryParameters.passwort as string);
                 }
+
             }
 
-            if (q.pathname == "/json") {
-                let stringJSON: string = JSON.stringify(q.query);
-                _response.write(stringJSON);
+            _response.end();
+            // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+        }
+
+        async function registriereUser(_user: User): Promise<void> {
+            //überprüfen ob es schon ein Konto mit der Mailadresse gibt
+            //countDocuments
+
+            let countDocuments: number = await user.countDocuments({ "email": _user.email });
+
+            if (countDocuments > 0) {
+                console.log("Mailadresse bereits vergeben");
+                //TODO: An Client weitergeben
+            } else {
+                await user.insertOne(_user);
+            }
+        }
+
+        async function anmelden(_email: string, _passwort: string): Promise<void> {
+
+            let countDocuments: number = await user.countDocuments({ "email": _email, "passwort": _passwort });
+
+            if (countDocuments > 0) {
+                console.log("angemeldet");
+            } else {
+                console.log("falsche Daten eingegeben");
             }
 
         }
 
-        _response.end();
-        // Es wird ein Header erstellt und da die request auf einer neuen Seite ausgegeben.
+
+
+
     }
-
-    interface User {
-        "vorname": string;
-        "nachname": string;
-        "e-mail": string;
-        "passwort": string;
-    }
-
-    let databaseUrl: string = "mongodb+srv://Beispiel_User:<password>@cluster2000.9tkvz.mongodb.net/<dbname>?retryWrites=true&w=majority";
-    let user: Mongo.Collection;
-
-
-}
